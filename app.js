1f773f936c87d39d1f0a86cd05eeadd84e175e6c
abdoul karim <abdoulkarimsoumana62@gmail.com>
2023-11-21 14:19:29 +0100

    first

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const small = document.getElementById("small");
 const btnBoutique = document.getElementById("btnBoutique");
 const divFill = document.getElementById("divFill");
 const btnExit = document.getElementById("btnExit");
+const tbody = document.getElementById("tbody");
+const ajouterAupanier = document.getElementById("ajouter");
+const Clear = document.getElementById("Clear");
+const iTems = document.getElementById("iTems");
 
 
-
-
-
-// small.addEventListener('mouseenter',function () {
-//      divFill.classList.remove("d-none")
-//      console.log("survoler");
-//  });
-//  small.addEventListener('mouseleave',function () {
-//      divFill.classList.add("d-none")
-//      console.log("De-survoler"); 
-//  })
+let tableChopingcard = [];
+if (!JSON.parse(localStorage.getItem("tableChopingcard"))) {
+    tableChopingcard = localStorage.setItem("tableChopingcard",JSON.stringify(tableChopingcard))
+}
+tableChopingcard = JSON.parse(localStorage.getItem("tableChopingcard"));
 
 btnBoutique.addEventListener('click',function () {
     divFill.classList.remove("d-none");
@@ -22,3 +20,32 @@ btnBoutique.addEventListener('click',function () {
 btnExit.addEventListener('click',function () {
     divFill.classList.add("d-none");
 });  
+Clear.addEventListener('click',function () {
+    localStorage.clear();
+    document.location.reload();
+})
+
+ ajouterAupanier.addEventListener('click',function (event) {
+    event.preventDefault();
+        tableChopingcard.push(datas);
+        localStorage.setItem("tableChopingcard",JSON.stringify(tableChopingcard));
+        ajoutElements();
+
+ });
+
+
+function ajoutElements(event) {
+   tbody.innerHTML = "";
+   tableChopingcard.forEach((element,index) => {
+    iTems.innerText = index+1;
+    tbody.innerHTML += `
+    <tr>
+       <td class="text-center w-25"><img  class= "imageAjout" src="${element.image}" alt=""></td>
+       <td>${element.namePrix}</td>
+       <td><i class="bi bi-trash"></i></td>
+   </tr>
+   `
+   });
+
+}
+ajoutElements();
---
+++ data.js
@@ -0,0 +1,5 @@
+const datas = {
+  namePrix:"Cake <br> $5",
+  image:"http://127.0.0.1:5502/Images/capkupes.new3.avif",
+}
+
