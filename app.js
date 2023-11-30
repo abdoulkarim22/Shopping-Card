620e9a0fc17f96d4412684929bee40cf80fa3672
abdoul karim <abdoulkarimsoumana62@gmail.com>
2023-11-30 13:38:51 +0100

    first

--- app.js
+++ app.js
@@ -12,40 +12,38 @@ const afficheTotal = document.getElementById("afficheTotal");
 // const filtre = document.querySelectorAll(".Sweet-Items");
 const theCard = document.getElementById("theCard")
 const idCake = document.getElementById("idCake");
-const divAffiche = document.getElementById("divAffiche")
-
+const divAffiche = document.getElementById("divAffiche");
 let total = 0;
 
+// ===================== search ==========================
+const search = () => {
+  const search = document.getElementById("input-search").value.toUpperCase();
+  const produits = document.querySelectorAll(".produits");
+  const allName = document.getElementsByTagName("h2");
 
+  for (let i = 0; i < allName.length; i++) {
+    let length = produits[i].getElementsByTagName("h2")[0];
 
-function affiche(array) {
-    divAffiche.innerHTML=""
-   array.forEach(element => {
-    divAffiche.innerHTML += `<div class="col Cupcake-Items">
-    <div class="card shadow-sm h-100">
-      <!-- =====================  img sector 1 ========================== -->
-       <img id="Cupcake" class="h-75 cardimg"  src="${element.image}" alt=""> 
-      <!-- =====================  img sector 1  ========================== -->
-      <div class="card-body bg-dark-subtle">
-       <div class="div-Star"><i class="bi bi-star-fill fs-4 " id="etoile"></i><i class="bi bi-star-fill fs-4"></i><i class="bi bi-star-fill fs-4"></i><i class="bi bi-star-fill fs-4"></i><i class="bi bi-star-fill fs-4"></i></div>
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="btn-group">
-           <p class="nameCupcake">${element.name}</p>
-          </div>
-          <small id="small" style="font-size: 22px;color: green;">$5<div class="p-2"><i onclick="ajoutElements(index)"; id="ajouter" class="bi bi-cart-fill fs-3" style="cursor: pointer;"></i></div></small>
-        </div>
-      </div>
-    </div>
-  </div>`
-   });
+    if (length) {
+      let textvalue = length.textContent || length.innerHTML
+      if (textvalue.toUpperCase().indexOf(search) > -1) {
+        console.log(textvalue.indexOf(search));
+        produits[i].style.display = ""
+      }else{
+        produits[i].style.display = "none"
+      }
+    }
+   
+    
+  }
+ 
 }
-affiche(datas);
+// ===================== search ==========================
 
-let tableChopingcard = [];
-if (!JSON.parse(localStorage.getItem("tableChopingcard"))) {
-    tableChopingcard = localStorage.setItem("tableChopingcard",JSON.stringify(tableChopingcard))
-}
-tableChopingcard = JSON.parse(localStorage.getItem("tableChopingcard"));
+
+
+
+
 
 btnBoutique.addEventListener('click',function () {
     divFill.classList.remove("d-none");
@@ -53,10 +51,10 @@ btnBoutique.addEventListener('click',function () {
 btnExit.addEventListener('click',function () {
     divFill.classList.add("d-none");
 });  
-Clear.addEventListener('click',function () {
-    localStorage.clear();
-    document.location.reload();
-});
+// Clear.addEventListener('click',function () {
+//     localStorage.clear();
+//     document.location.reload();
+// });
 
 
 idCake.addEventListener('click',function (event) {
@@ -65,41 +63,76 @@ idCake.addEventListener('click',function (event) {
     arrayFilter = datas.filter((element) => element.categorie === "Cake");
     affiche(arrayFilter);
 });
-const ajouterAupanier = document.getElementById("ajouter");
 
-  ajouterAupanier.addEventListener('click',function (event) {
-     alert("Ajout fait avec succes");
-     event.preventDefault();
-         tableChopingcard.push(datas);
-         localStorage.setItem("tableChopingcard",JSON.stringify(tableChopingcard));
-         ajoutElements();
-  });
+function affiche(array) {
+  divAffiche.innerHTML= ""
+ array.forEach((element,index) => {
+  divAffiche.innerHTML += `<div class="col">
+  <div class="card shadow-sm h-100 produits">
+    <!-- =====================  img sector 1 ========================== -->
+     <img id="Cupcake" class="h-75 cardimg"  src="${element.image}" alt=""> 
+    <!-- =====================  img sector 1  ========================== -->
+    <div class="card-body bg-dark-subtle">
+     <div class="div-Star"><i class="bi bi-star-fill fs-4 " id="etoile"></i><i class="bi bi-star-fill fs-4"></i><i class="bi bi-star-fill fs-4"></i><i class="bi bi-star-fill fs-4"></i><i class="bi bi-star-fill fs-4"></i></div>
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="btn-group">
+         <h2 class="nameCupcake">${element.name}</h2>
+        </div>
+        <small id="small" style="font-size: 22px;color: green;">${element.prix}<div class="p-2"><i onclick="ajoutElements(${index})"; id="ajouter" class="bi bi-cart-fill fs-3" style="cursor: pointer;"></i></div></small>
+      </div>
+    </div>
+  </div>
+</div>`
+ });
+}
+affiche(datas);
+
+// const ajouterAupanier = document.querySelectorAll("#ajouter");
+
+//   ajouterAupanier.addEventListener('click',function (event) {
+//      alert("Ajout fait avec succes");
+//      event.preventDefault();
+//          tableChopingcard.push(datas);
+//          localStorage.setItem("tableChopingcard",JSON.stringify(tableChopingcard));
+//          ajoutElements();
+//   });
+
+let tableChopingCard = [];
+if (!JSON.parse(localStorage.getItem("tableChopingCard"))) {
+  tableChopingCard = localStorage.setItem("tableChopingCard",JSON.stringify(tableChopingCard));
+}
+tableExpenses = JSON.parse(localStorage.getItem("tableExpenses"));
 
 
-function ajoutElements(event) {
+function ajoutElements(index) {
+   tableChopingCard.push(datas[index]);
+   localStorage.setItem("tableChopingCard",JSON.stringify(tableChopingCard));
+   displayselements(); 
+}
+  function displayselements(event) {
     event?.preventDefault();
+   console.log("No");
    tbody.innerHTML = "";
-   let total = 0;
-   tableChopingcard.forEach((element,index) => {
-    total += parseInt(element.prix);
-    console.log(total);
-    iTems.innerText = index+1;
+    // iTems.innerText = index+1; 
+  tableChopingCard.forEach((element,index) => {
     tbody.innerHTML += `
     <tr>
-       <td class="text-center w-25"><img  class= "imageAjout" src="${element.image}" alt=""></td>
-       <td>${element.namePrix}</td>
-       <td><i onclick="trash(${index})" class="bi bi-trash"></i></td>
+       <td class="text-center w-25"><img  class= "imageAjout" src="${tableChopingCard[index].image}" alt=""></td>
+       <td>${tableChopingCard[index].name}</td>
+       <td class="text-center p-3">${tableChopingCard[index].prix}</td>
+       <td><i onclick="trash(${index})" class="bi bi-trash fs-3"></i></td>
    </tr>`
-   });
-   afficheTotal.innerText = total
-   totalAchat.innerText = total
+  });
+  localStorage.setItem("tableChopingCard",JSON.stringify(tableChopingCard));
 
-}
-ajoutElements();
+
+ }
+  displayselements(); 
+
 
 function trash(index) {
-    tableChopingcard.splice(index);
-    localStorage.setItem("tableChopingcard",JSON.stringify(tableChopingcard));
-    ajoutElements();
-  }
- 
+  tableChopingCard.splice(index,1);
+  localStorage.setItem("tableChopingcard",JSON.stringify(datas[index]));
+  displayselements();
+}
+
--- data.js
+++ data.js
@@ -1,74 +1,74 @@
 const datas = [
   {
     name:"Cupcake Items",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images/capkupes.new3.avif",
     categorie: "Cupcake"
   },
   {
     name:"Sweet Items",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images/New.sweets.webp",
     categorie:"Sweet"
   },
   {
     name:"Cake Items",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images/5.jpg",
     categorie:"Cake"
   },
 
   {
     name:"Cake Items",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images/1.jpg",
     categorie:"Cake"
   },
   {
     name: "Cupcake Items",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images//cupkapes.New2.webp",
     categorie:"Cupcake"
   },
   {
     name:"Cupcake Items",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images/Cupkapes.New.webp",
     categorie:"Cupcake"
   },
   {
     name:"Sweet Items",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images/Sweets.1.avif",
     categorie:"Sweet"
   },
   {
     name:"Cake Items",
-    prix:"10",
+    prix:"$10",
     image:"http://127.0.0.1:5503/Images/sweets.2.webp",
     categorie:"Cake"
   },
   {
     name:"Dougnut Item",
-    prix:"15",
+    prix:"$15",
     image:"http://127.0.0.1:5503/Images/Doun.new3.jpeg",
     categorie:"Dougnut"
   },
   {
     name: "Dougnut Item",
-    prix:"5",
+    prix:"$5",
     image:"http://127.0.0.1:5503/Images/Doun.new.webp",
     categorie:"Dougnut"
   },
   {
     name:"Dougnut Item",
-    prix:"10",
+    prix:"$10",
     image:"http://127.0.0.1:5503/Images/Doun.new2.webp",
     categorie:"Dougnut"
   },
   {
     name:"Sweet Items",
-    prix:"15",
+    prix:"$15",
     image:"http://127.0.0.1:5503/Images/Sweets.3.webp",
     categorie:"Sweet"
   },
